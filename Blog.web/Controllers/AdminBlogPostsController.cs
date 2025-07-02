5df0af6a777a61045f3bf86b009b342c9724de38
Hamdi Slimi <slimihamdi1234@gmail.com>
2025-07-02 02:20:28 -0600

    create new blogpost inside the DB

---
+++ Blog.web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,33 @@
+﻿using Blog.web.Models.ViewModels;
+using Blog.web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository tagRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository) 
+        {
+            this.tagRepository = tagRepository;
+        }
+        [HttpGet]
+        public async Task <IActionResult> Add()
+        {
+            //get Tags from repository
+            var tags = await tagRepository.GetAllAsync();
+                var  model = new AddBlogPostRequest
+                {
+                    Tags = tags.Select(x => new SelectListItem { Text = x.DisplayName, Value = x.Id.ToString() })
+                };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            return RedirectToAction("Add");
+        }
+    }
+}
